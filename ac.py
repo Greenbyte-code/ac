a7eb8afbfae35fb69aeda113b1363e779bfb1dbb
Greenbyte <93258461+Greenbyte-code@users.noreply.github.com>
2022-05-15 13:55:56 +0200

    Add files via upload

---
+++ ac.py
@@ -0,0 +1,11 @@
+import mouse
+import keyboard
+
+while True:
+    if keyboard.is_pressed('v'):
+        mouse.click(button='left')
+
+#0. Python insterieren
+#1. cmd öffnen
+#2.     pip install mouse
+#2.5    pip install keyboard
